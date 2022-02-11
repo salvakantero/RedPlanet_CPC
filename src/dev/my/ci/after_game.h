f0af680feb4d49ea25579b97ee753d80b29d6db6
salvakantero <salvacantero@outlook.com>
2022-02-11 17:40:50 +0100

    -

---
+++ src/dev/my/ci/after_game.h
@@ -0,0 +1,8 @@
+// MTE MK1 (la Churrera) v5.0
+// Copyleft 2010-2014, 2020 by the Mojon Twins
+
+// This code is ran just before looping back to the title screen
+
+// custom salva
+// palette 1
+pal_set (pal_1);
