7139407bff9dad74099a4252e72b07ab40ea519d
salvakantero <salvacantero@outlook.com>
2022-02-16 20:55:26 +0100

    -

--- src/dev/wyz_player.h
+++ src/dev/wyz_player.h
@@ -2,7 +2,7 @@
 // This needs a buffer to decompress songs.
 // Biggest .mus file must fit, so adjust if needed:
 
-#define WYZ_SONG_BUFFER 0x8B5C
+#define WYZ_SONG_BUFFER 0x8800
 #define BASE_WYZ 		0xDF80
 
 void wyz_init (void) {
