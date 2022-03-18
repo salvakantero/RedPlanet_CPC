2b941f8e29d3f3125418a8e2d688ffb5cccedc86
salvakantero <salvacantero@outlook.com>
2022-03-18 20:31:55 +0100

    -

--- src/dev/loader/loadercpc.asm
+++ src/dev/loader/loadercpc.asm
@@ -30,13 +30,13 @@ org $BA00
 
 ; Load binary
 
-	ld      ix, $BA00 - 17452
-	ld      de, 17452
+	ld      ix, $BA00 - 17652
+	ld      de, 17652
 	call    cpct_miniload_asm
 
 ; Depack binary
 
-	ld      hl, $BA00 - 17452
+	ld      hl, $BA00 - 17652
 	ld      de, $0400
 	call    dzx7_standard
 
