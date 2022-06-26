1b6da1dcf6d56a6e56686b3b0ec333277b34a31d
salvakantero <salvacantero@outlook.com>
2022-06-26 13:17:30 +0200

    -

--- src/dev/my/fixed_screens.h
+++ src/dev/my/fixed_screens.h
@@ -2,30 +2,36 @@
 // Copyleft 2010-2014, 2020 by the Mojon Twins
 
 void game_ending (void) {
-	blackout ();
 	// custom salva
+	//blackout ();
 	#ifdef MODE_128K
 		// Resource 2 = ending
 		get_resource (ENDING_BIN, BASE_SUPERBUFF);
 	#else
 		//unpack ((unsigned int) (s_ending), BASE_SUPERBUFF);
 	#endif
-	cpc_ShowTileMap (1);
+	//cpc_ShowTileMap (1);
 	
+	// final percentage
+	_x = 29;
+	_y = 2;
+	_t = 99;
+	print_number2();
+
 	#ifdef ENGLISH_LANG
 		//_x = 5; _y =  9; _gp_gen = " !                   ! "; print_str();
 		_x = 5; _y = 10; _gp_gen = " !      MISSION      ! "; print_str();
 		_x = 5; _y = 11; _gp_gen = " !   ACCOMPLISHED.   ! "; print_str();
 		_x = 5; _y = 12; _gp_gen = " !     ITS TIME      ! "; print_str();
 		_x = 5; _y = 13; _gp_gen = " !    TO GO HOME.    ! "; print_str();
-		_x = 5; _y = 14; _gp_gen = " ! .::RED PLANET2::. ! "; print_str();
+		//_x = 5; _y = 14; _gp_gen = " ! .::RED PLANET2::. ! "; print_str();
 	#else
 		//_x = 5; _y =  9; _gp_gen = " !                   ! "; print_str();
 		_x = 5; _y = 10; _gp_gen = " ! OBJETIVO CUMPLIDO ! "; print_str();
 		_x = 5; _y = 11; _gp_gen = " !                   ! "; print_str();
 		_x = 5; _y = 12; _gp_gen = " !    ES HORA DE     ! "; print_str();
 		_x = 5; _y = 13; _gp_gen = " !  REGRESAR A CASA  ! "; print_str();
-		_x = 5; _y = 14; _gp_gen = " ! .::RED PLANET2::. ! "; print_str();
+		//_x = 5; _y = 14; _gp_gen = " ! .::RED PLANET2::. ! "; print_str();
 	#endif
 
 	cpc_UpdateNow (0);
